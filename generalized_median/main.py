bc0ca076b10c2bcbd2f84ba4f682a954dd1fbfb8
Stefan Heid <stefan.heid@upb.de>
2023-09-06 17:29:38 +0200

    improved kendal mean

--- generalized_median/main.py
+++ generalized_median/main.py
@@ -30,7 +30,7 @@ def generalized_rank_median_spearman(rankings: np.array):
     return rankdata(np.array(rankings).mean(axis=0), method="ordinal")
 
 
-def generalized_rank_median_kendal(rankings):
+def generalized_rank_median_kendal(rankings: np.array):
     """
     Calculates the generalized mean of a population of rankings.
     Generalized means, the median must not necessarily be part of the original data.
@@ -43,19 +43,23 @@ def generalized_rank_median_kendal(rankings):
     # convert all rankings to orderings
     orderings = to_ordering(data)
 
-    def calculate_pairwise_comparisons(ordering):
+    def calculate_pairwise_comparisons(ordering: np.array) -> np.array:
+        """
+        :param ordering: 1 × n array of an ordering
+        :return: 1 × (n over 2) pairwise comparisons. 1 if a<b and a≺b, -1 otherwise
+        """
         # upper triangle matrix to calculate all pairwise comparisons
         pair_indices = np.array(np.triu_indices(ranklength, 1))
-        return np.sign(np.array([-1, 1]) @ ordering[pair_indices])
+        return np.sign(np.array([-1, 1]) @ ordering[pair_indices])  # 1 × (ranklength over 2)
 
     # calculate all pairwise comparisons for all orderings
-    pairs = np.apply_along_axis(calculate_pairwise_comparisons, 1, orderings)
-    average_agreement = pairs.mean(0)
+    pairs = np.apply_along_axis(calculate_pairwise_comparisons, 1, orderings)  # n × (ranklength over 2)
+    average_pairwise_ordering = pairs.mean(0)  # 1 × (ranklength over 2)
 
     # reshape the pairs back into a matrix
     pairwise_ranks_as_matrix = np.zeros([ranklength, ranklength])
-    pairwise_ranks_as_matrix[np.triu_indices(ranklength, 1)] = average_agreement
-    pairwise_ranks_as_matrix[np.tril_indices(ranklength, -1)] = -average_agreement
+    pairwise_ranks_as_matrix[np.triu_indices(ranklength, 1)] = average_pairwise_ordering
+    pairwise_ranks_as_matrix -= pairwise_ranks_as_matrix.T  # fill lower triangle with complement values
 
     # collect average rank as the mean of pairwise comparisons
     median_ranking = rankdata(-pairwise_ranks_as_matrix.mean(1), method="ordinal")
@@ -63,6 +67,6 @@ def generalized_rank_median_kendal(rankings):
 
 
 if __name__ == '__main__':
-    rankings = [[1, 2, 3, 4], [1, 2, 4, 3], [1, 4, 2, 3], [1, 3, 2, 4]]
+    rankings = [[1, 2, 3, 4], [4, 2, 1, 3], [1, 4, 2, 3], [1, 3, 2, 4]]
     mean = generalized_rank_median_kendal(rankings)
     print(mean)
