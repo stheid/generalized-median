ffc563de6f3d6728800eb7a6fa155cf55852d0ca
Stefan Heid <stefan.heid@upb.de>
2023-09-06 15:29:16 +0200

    implemented generalized median for kendal

--- generalized_median/main.py
+++ generalized_median/main.py
@@ -19,35 +19,50 @@ def generalized_rank_median(data: np.array, function=None):
     return None
 
 
-def generalized_rank_median_spearman(data: np.array):
+def generalized_rank_median_spearman(rankings: np.array):
     """
     Calculates the generalized mean of a population of rankings.
     Generalized means, the median must not necessarily be part of the original data.
 
-    :param data: Array-like list of full rankings to calculate the median of. Each row is supposed to be a ranking
+    :param rankings: Array-like list of full rankings to calculate the median of. Each row is supposed to be a ranking
     :return: Generalized mean ranking
     """
-    return rankdata(np.array(data).mean(axis=0), method="ordinal")
+    return rankdata(np.array(rankings).mean(axis=0), method="ordinal")
 
 
-def kendal(ranking_pair):
+def generalized_rank_median_kendal(rankings):
     """
-    operates on rankings
-    :param r1:
-    :param r2:
-    :return:
+    Calculates the generalized mean of a population of rankings.
+    Generalized means, the median must not necessarily be part of the original data.
+
+    :param rankings: Array-like list of full rankings to calculate the median of. Each row is supposed to be a ranking
+    :return: Generalized mean ranking
     """
-    data = np.array(ranking_pair)
+    data = np.array(rankings)
     n, ranklength = data.shape
     # convert all rankings to orderings
     orderings = to_ordering(data)
-    pair_indices = np.array(np.triu_indices(ranklength, 1))
-    pairs = np.apply_along_axis(lambda a: np.sign(np.array([-1, 1]) @ a[pair_indices]), 1, orderings)
-    kendal_correlation = (pairs.sum(0) - 1).mean()
-    return kendal_correlation
+
+    def calculate_pairwise_comparisons(ordering):
+        # upper triangle matrix to calculate all pairwise comparisons
+        pair_indices = np.array(np.triu_indices(ranklength, 1))
+        return np.sign(np.array([-1, 1]) @ ordering[pair_indices])
+
+    # calculate all pairwise comparisons for all orderings
+    pairs = np.apply_along_axis(calculate_pairwise_comparisons, 1, orderings)
+    average_agreement = pairs.mean(0)
+
+    # reshape the pairs back into a matrix
+    pairwise_ranks_as_matrix = np.zeros([ranklength, ranklength])
+    pairwise_ranks_as_matrix[np.triu_indices(ranklength, 1)] = average_agreement
+    pairwise_ranks_as_matrix[np.tril_indices(ranklength, -1)] = -average_agreement
+
+    # collect average rank as the mean of pairwise comparisons
+    median_ranking = rankdata(-pairwise_ranks_as_matrix.mean(1), method="ordinal")
+    return median_ranking
 
 
 if __name__ == '__main__':
-    rankings = [[1, 2, 3, 4], [1, 2, 4, 3] , [1,4,2,3]]
-    mean = kendal(rankings)
+    rankings = [[1, 2, 3, 4], [1, 2, 4, 3], [1, 4, 2, 3], [1, 3, 2, 4]]
+    mean = generalized_rank_median_kendal(rankings)
     print(mean)
